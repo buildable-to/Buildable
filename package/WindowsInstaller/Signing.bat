f251e81bbfd127db72bb96cd3a65b1bf53a33e97
Luka Lortkipanidze <lukalortkipanidze1@gmail.com>
2026-01-27 23:33:50 +0400

    Rebrand signing config to Buildable

--- package/WindowsInstaller/Signing.bat
+++ package/WindowsInstaller/Signing.bat
@@ -1,2 +1,2 @@
-signtool.exe sign /f FCweborg.pfx /p FreeCADIsCool /fd sha512 /tr http://timestamp.digicert.com /td sha512 /v %1
+signtool.exe sign /f Buildable.pfx /p BuildableSign /fd sha512 /tr http://timestamp.digicert.com /td sha512 /v %1
 certutil -hashfile %1 SHA256 > %1-SHA256.txt
